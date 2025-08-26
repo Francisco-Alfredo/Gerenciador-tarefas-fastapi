b1eac42b6d0992f920e175db241b2fd56df912d6
Francisco-Alfredo <sanumbofrancisco@gmail.com>
2025-08-26 08:04:17 +0100

    Implementando rotas de CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,57 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
+
+from fast_zero.schemas import (
+    MessageSchema,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
-app.mount("/coverage", StaticFiles(directory="htmlcov"), name="coverage")
+app.mount('/coverage', StaticFiles(directory='htmlcov'), name='coverage')
 
-@app.get('/')
+
+database = []
+
+
+@app.get('/', status_code=200, response_model=MessageSchema)
 def read_root():
     return {'message': 'Welcome to the Fast Zero API!'}
+
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=404, detail='User not found')
+
+    index = user_id - 1
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[index] = user_with_id
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=MessageSchema)
+def delete_user(user_id: int):
+    if user_id < len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+    database.pop(user_id - 1)
+    return {'message': 'User deleted successfully'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,14 +2,72 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+from fast_zero.fast_zero.app import app
 
 client = TestClient(app)
 
-def test_root_endpoint():
+
+def test_root_endpoint(client):
     client = TestClient(app)
 
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Welcome to the Fast Zero API!'}
+
+
+def test_create_user(client):
+    client = TestClient(app)
+
+    response = client.post(
+        '/users',
+        json={
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'testuser@example.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testuser',
+                'email': 'testuser@example.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'newsecret',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted successfully'}
